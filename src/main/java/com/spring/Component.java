5c61b4688365b9818284ccd03153fdc7a2c98701
Lovsog <840018730@qq.com>
2022-06-05 17:55:40 +0800

    零件注解

---
+++ src/main/java/com/spring/Component.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Lovsog
+ * 2022/6/5
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Component {
+
+    String value() default "";
+
+}
