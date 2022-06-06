7619bfdff13a27a8370d7313b3632e9f6a464033
Lovsog <840018730@qq.com>
2022-06-06 17:35:09 +0800

    容器类

--- src/main/java/com/spring/MyApplicationContext.java
+++ src/main/java/com/spring/MyApplicationContext.java
@@ -18,15 +18,16 @@ public class MyApplicationContext {
         // ComponentScan注解 --> 扫描路径 --> 扫描
         ComponentScan componentScanAnnotation = (ComponentScan) configClass.getDeclaredAnnotation(ComponentScan.class);
         String path = componentScanAnnotation.value();  //获得扫描路径 com.lovsog.service
+        path = path.replace(".", "/");
 
         // 2. 扫描
         // 3种类加载器：
-        // Bootstrap    ---> jre/lib
-        // Ext          ---> jre/ext/lib
-        // App          ---> classpath          ---> 当前应用类加载器对应的路径
+        //      Bootstrap    ---> jre/lib
+        //      Ext          ---> jre/ext/lib
+        //      App          ---> classpath          ---> 当前应用类加载器对应的路径
         ClassLoader classLoader = MyApplicationContext.class.getClassLoader(); // 获得App类加载器
         //classLoader.getResource("com/lovsog/service"); // 这里是相对路径，相对上面的classpath
-        URL resource = classLoader.getResource("com/lovsog/service"); // 这里是相对路径，相对上面的classpath
+        URL resource = classLoader.getResource(path); // 这里是相对路径，相对上面的classpath
         File file = new File(resource.getFile());
         if (file.isDirectory()) {
             File[] files = file.listFiles();
@@ -37,7 +38,7 @@ public class MyApplicationContext {
                     String className = fileName.substring(fileName.indexOf("com"), fileName.indexOf(".class")); // 将绝对路径转换为 com.lovsog.service.UserService 的格式
                     className = className.replace("\\", ".");
                     try {
-                        Class<?> clazz = classLoader.loadClass("");
+                        Class<?> clazz = classLoader.loadClass(className);
                         // 判断得到的类上是否有Component注解，如果有，再继续操作
                         if (clazz.isAnnotationPresent(Component.class)) {
                             //..
