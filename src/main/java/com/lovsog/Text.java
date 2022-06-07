f100097afea690c8349ab00119371aac16fff644
Lovsog <840018730@qq.com>
2022-06-07 22:46:39 +0800

    测试

--- src/main/java/com/lovsog/Text.java
+++ src/main/java/com/lovsog/Text.java
@@ -9,7 +9,11 @@ import com.spring.MyApplicationContext;
 public class Text {
     public static void main(String[] args) {
         MyApplicationContext applicationContext = new MyApplicationContext(AppConfig.class);
-        Object userService = applicationContext.getBean("userService");
+
+        // Object userService = applicationContext.getBean("userService");     // map <beanName, bean对象>  <---单例池
+        System.out.println(applicationContext.getBean("userService"));
+        System.out.println(applicationContext.getBean("userService"));
+        System.out.println(applicationContext.getBean("userService"));
 
     }
 }
