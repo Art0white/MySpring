9d9033c68f7a09a9b5edcd4f19fe63c0e6d0e8df
Lovsog <840018730@qq.com>
2022-06-07 23:13:34 +0800

    4 作为UserService的属性

---
+++ src/main/java/com/lovsog/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.lovsog.service;
+
+import com.spring.Autowired;
+import com.spring.Component;
+
+/**
+ * @author Lovsog
+ * 2022/6/7
+ **/
+@Component("orderService")
+public class OrderService {
+
+
+
+}
