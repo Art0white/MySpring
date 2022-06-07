9a4ce9198bb406e64f579769df5326d91ce40fa2
Lovsog <840018730@qq.com>
2022-06-07 22:46:54 +0800

    设置作用域

--- src/main/java/com/lovsog/service/UserService.java
+++ src/main/java/com/lovsog/service/UserService.java
@@ -1,12 +1,14 @@
 package com.lovsog.service;
 
 import com.spring.Component;
+import com.spring.Scope;
 
 /**
  * @author Lovsog
  * 2022/6/5
  **/
 @Component("userService")
+@Scope("prototype")
 public class UserService {
 
 }
