b58d7ac40adf82a4f08d9873e97943e0ecfaa03f
Lovsog <840018730@qq.com>
2022-06-13 22:19:49 +0800

    8 AOP实现

--- src/main/java/com/lovsog/service/LovsogBeanPostProcessor.java
+++ src/main/java/com/lovsog/service/LovsogBeanPostProcessor.java
@@ -4,6 +4,10 @@ import com.lovsog.service.UserService;
 import com.spring.BeanPostProcessor;
 import com.spring.Component;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * @author Lovsog
  * 2022/6/8
@@ -26,6 +30,22 @@ public class LovsogBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) {
         // 初始化后自定义操作
         System.out.println("初始化后");
+        // 匹配
+        if (beanName.equals("userService")) {
+
+            //生成代理对象
+            Object proxyInstance = Proxy.newProxyInstance(LovsogBeanPostProcessor.class.getClassLoader(), bean.getClass().getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    // 执行代理逻辑
+                    System.out.println("代理逻辑"); // 找切点
+                    // 执行业务逻辑
+                    return method.invoke(bean, args);
+                }
+            });
+            // 返回代理对象
+            return proxyInstance;
+        }
         return bean;
     }
 }
